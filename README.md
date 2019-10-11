c2d2d3590c68e2ee7f29d249d2a2df16e402a374
葒茶菌 <kouchyakun@gmail.com>
2019-10-11 20:12:12 +0800

    support async and sync usages

--- README.md
+++ README.md
@@ -13,8 +13,33 @@ npm i en-etym
 ## Usage
 
 ```javascript
-const etym = require('etym')
+const etym = require('en-etym')
+```
 
-etym.getEtym('glitter', etymList => console.log(JSON.stringify(etymList)))
+## API
+
+### etym.getEtym
+
+> (word: String) => Promise
+
+Fetch etymology information of a word in an asynchronized way. The resolved value of the Promise is the array of etymology information.
+
+```javascript
+const etym = require('en-etym')
+
+etym.getEtym('glitter').then(etymList => console.log(JSON.stringify(etymList)))
+// [{"title":"glitter (v.)","etymDict":{"Old Norse":["glitra"],"Proto-Germanic":["*glit-"],"Old English":["glitenian"],"PIE":["*ghleid-","*ghel-"],"Greek":["khlidon"]}}]
+```
+
+### etym.getEtymSync
+
+> (word: String, callback: (etymList) => any) => void
+
+Fetch etymology information of a word in a synchronized way. The argument of the callback function is the array of etymology information.
+
+```javascript
+const etym = require('en-etym')
+
+etym.getEtymSync('glitter', etymList => console.log(JSON.stringify(etymList)))
 // [{"title":"glitter (v.)","etymDict":{"Old Norse":["glitra"],"Proto-Germanic":["*glit-"],"Old English":["glitenian"],"PIE":["*ghleid-","*ghel-"],"Greek":["khlidon"]}}]
 ```
--- src/index.js
+++ src/index.js
@@ -5,8 +5,63 @@ const iconv = require('iconv-lite')
 const url = 'https://www.etymonline.com/word/'
 const languages = [/(Old French)$/, /(Anglo-French)$/, /(Old North French)$/, /(Old English)$/, /(PIE root)$/, /(PIE)$/, /(root)$/, /(Proto-Germanic)$/, /(Gallo-Roman)$/, /(Old Norse)$/, /(Modern Latin)$/, /(Medieval Latin)$/, /(Late Latin)$/, /(Latin)$/, /(Greek)$/, /(French)$/]
 
-function getEtym (word, callback) {
+function _parseHTML (html) {
   let etymList = []
+  const $ = cheerio.load(html, { decodeEntities: false })
+  const allEntries = $('[class^="word--"]')
+  $(allEntries).each((entryId, entryItem) => {
+    const title = $($(entryItem).find('[class^="word__name--"]')[0]).text().trim()
+    let etymDict = {}
+    const paragraphs = $(entryItem).find('[class^="word__defination--"] p')
+    $(paragraphs).each((pid, paraItem) => {
+      $(paraItem).children().each((i, item) => {
+        if ($(item).hasClass('notranslate')) {
+          const lastText = item.prev.data.trim()
+          for (const langRegExp of languages) {
+            const groups = langRegExp.exec(lastText)
+            if (groups && groups.length > 1) {
+              let key = groups[1]
+              if (['PIE', 'root'].includes(key)) {
+                key = 'PIE'
+              }
+              if (etymDict[key]) {
+                etymDict[key].push($(item).text().trim())
+              } else {
+                etymDict[key] = [$(item).text().trim()]
+              }
+              break
+            }
+          }
+        }
+      })
+    })
+    if (Object.keys(etymDict).length > 0) {
+      etymList.push({ title, etymDict })
+    }
+  })
+  return etymList
+}
+
+function getEtym (word) {
+  let etymList = []
+  return new Promise((resolve, reject) => {
+    https.get(url + word, res => {
+      let chunks = []
+      res.on('data', chunk => {
+        chunks.push(chunk)
+      })
+      res.on('end', () => {
+        const html = iconv.decode(Buffer.concat(chunks), 'utf-8')
+        etymList = _parseHTML(html)
+        resolve(etymList)
+      })
+    }).on('error', e => {
+      resolve([])
+    })
+  })
+}
+
+function getEtymSync (word, callback) {
   https.get(url + word, res => {
     let chunks = []
     res.on('data', chunk => {
@@ -14,37 +69,7 @@ function getEtym (word, callback) {
     })
     res.on('end', () => {
       const html = iconv.decode(Buffer.concat(chunks), 'utf-8')
-      const $ = cheerio.load(html, { decodeEntities: false })
-      const allEntries = $('[class^="word--"]')
-      $(allEntries).each((entryId, entryItem) => {
-        const title = $($(entryItem).find('[class^="word__name--"]')[0]).text().trim()
-        let etymDict = {}
-        const paragraphs = $(entryItem).find('[class^="word__defination--"] p')
-        $(paragraphs).each((pid, paraItem) => {
-          $(paraItem).children().each((i, item) => {
-            if ($(item).hasClass('notranslate')) {
-              const lastText = item.prev.data.trim()
-              for (const langRegExp of languages) {
-                const groups = langRegExp.exec(lastText)
-                if (groups && groups.length > 1) {
-                  let key = groups[1]
-                  if (['PIE', 'root'].includes(key)) {
-                    key = 'PIE'
-                  }
-                  if (etymDict[key]) {
-                    etymDict[key].push($(item).text().trim())
-                  } else {
-                    etymDict[key] = [$(item).text().trim()]
-                  }
-                  break
-                }
-              }
-            }
-          })
-        })
-        etymList.push({ title, etymDict })
-      })
-      callback && callback(etymList.filter(item => Object.keys(item.etymDict).length > 0))
+      callback && callback(_parseHTML(html))
     })
   }).on('error', e => {
     callback && callback([])
@@ -52,5 +77,6 @@ function getEtym (word, callback) {
 }
 
 module.exports = {
-  getEtym
+  getEtym,
+  getEtymSync
 }
